3af41580290aa2d46309737247262c393dc904f4
Chad Crowe <ccrowe@linkedin.com>
2019-04-21 10:34:30 -0500

    Finished first attempt at the problem

--- src/matching_socks/core.clj
+++ src/matching_socks/core.clj
@@ -1,9 +1,18 @@
 (ns matching-socks.core
   (:gen-class))
 
+(defn divide-by-two [val]
+(int (/ val 2)))
+
+(defn get-pairs [input]
+  (def frequ (frequencies input))
+  (def even-counts (map divide-by-two (vals frequ)))
+  (reduce + even-counts))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-
-(println "Hi")
+  (def input [10 20 20 10 10 30 50 10 20])
+(println (get-pairs input))
   )
